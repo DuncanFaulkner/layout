f5cc582dcb642bbad0f1ab659161d3e6fffef274
Duncan Faulkner <info@e-ips.com>
2021-01-24 16:38:56 +0000

    fix(update): update to latest versions

---
+++ src/setup-jest.ts
@@ -0,0 +1,9 @@
+import 'jest-preset-angular';
+// Object.defineProperty(document.body.style, 'transform', {
+//   value: () => {
+//     return {
+//       enumerable: true,
+//       configurable: true,
+//     };
+//   },
+// });
